67eb7b0d0bc57bd0f04473f7ab08de395425634a
kikimo <451943+kikimo@users.noreply.github.com>
2021-12-06 11:18:43 +0800

    make raft option global available

--- cmd/root.go
+++ cmd/root.go
@@ -115,7 +115,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.Flags().StringArrayVarP(&raftPeers, "peers", "e", defaultRaftPeers, "specify raft peers")
+	rootCmd.PersistentFlags().StringArrayVarP(&raftPeers, "peers", "e", defaultRaftPeers, "specify raft peers")
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 }
